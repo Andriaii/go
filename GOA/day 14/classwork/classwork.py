0da6205e8f90132b76094a15f6977d094b1b2a29
Andriaii <159134102+Andriaii@users.noreply.github.com>
2024-03-09 09:52:35 +0400

    Add files via upload

---
+++ GOA/day 14/classwork/classwork.py
@@ -0,0 +1,24 @@
+# program = ["python" , "javascript" , "c+++"]
+# print(program)
+
+
+# favourites = ["jujutsu kaisen" , "one piece" , "demon slayer"]
+
+# print(favourites)
+
+# favourites = ["jujutsu kaisen" , "one piece" , "demon slayer"]
+# the_best = favourites[0]
+# print(the_best)
+
+
+# car_brand = ["lamborghini" , "bmw" , "ferrari"]
+# print(car_brand)
+
+from turtle import *
+def draw_gate():
+    castle.penup(125)
+    castle.goto(100, -150)
+    castle.pendown(320)
+    castle.begin_fill(100)
+    draw_triangle(50)
+    castle.end_fill(120)
